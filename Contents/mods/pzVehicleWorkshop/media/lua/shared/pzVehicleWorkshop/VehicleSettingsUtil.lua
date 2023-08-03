c27d933dc4ef2b3e46f1afd73c50d6ca13f4787d
Poltergeistzx <igor.mov0@gmail.com>
2023-08-03 17:45:28 +0300

    rename file

--- Contents/mods/pzVehicleWorkshop/media/lua/shared/pzVehicleWorkshop/VehicleSettingsUtil.lua
+++ Contents/mods/pzVehicleWorkshop/media/lua/shared/pzVehicleWorkshop/VehicleSettingsUtil.lua
@@ -1,9 +1,9 @@
 local pzVehicleWorkshop = pzVehicleWorkshop
 local VehicleUtil = require "pzVehicleWorkshop/VehicleUtil"
 
-pzVehicleWorkshop.SettingsUtil = {}
+pzVehicleWorkshop.VehicleSettingsUtil = {}
 
-function pzVehicleWorkshop.SettingsUtil.checkFlipLamps(settings,vehicle,player,open)
+function pzVehicleWorkshop.VehicleSettingsUtil.checkFlipLamps(settings,vehicle,player,open)
     if settings.flipLamps ~= nil then
         VehicleUtil.DoorAnimOnServer(vehicle,vehicle:getPartById(settings.flipLamps),player,open)
     end
